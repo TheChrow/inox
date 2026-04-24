60bb66c286f10d2b2715e73d46b8a487b40e6b10
TheChrow <wherrera@gmail.com>
2026-04-24 14:38:20 -0400

    inox apps started

---
+++ infrastructure/db/models/documents_db.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Document(models.Model):
+    
+    doc_entry  = models.IntegerField(null=True)
+    doc_num = models.IntegerField(null=True)
+    folio = models.IntegerField(null=True)
+    date_document = models.DateField(null=True)
+    delivery_date = models.DateField(null=True)
+    delivery_schedule = models.DateField(null=True)
+    reference = models.CharField(max_length=255, null=True)
+    comment = models.TextField(null=True)
+    total_before_discount = models.FloatField()
+    discount = models.FloatField()
+    total_document = models.FloatField()
+    code_sale = models.IntegerField()
+    office_address = models.CharField(max_length=255, null=True)
+    address_delivery = models.CharField(max_length=255, null=True)
+    related_docEntry = models.IntegerField(null=True)
+    document_status = models.CharField(max_length=50, null=True)
+    """
+    foreign key
+
+    sale_type_id
+    tax_document_type_id
+    delivery_type_id
+    seller_id
+    description_id
+    payment_terms_id
+    business_partner_id
+
+    """
